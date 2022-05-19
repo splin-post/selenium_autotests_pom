694490e1dccf4456159e3dc780e79fbe5c9ee297
Sergei P <spin.el@yandex.ru>
2022-05-19 17:19:41 +0300

    Add new files into folder pages: base_page.py main_page.py and modified: test_main_page.py

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='es',
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: en, es, fr")
 
 
